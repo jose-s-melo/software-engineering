696128796e2c4577164a088fa1562ece7f9373d4
José Melo <jos.ryan.melo@gmail.com>
2026-05-01 19:04:00 -0300

    init project structure

---
+++ projeto-novo/backend/core/src/test/java/com/dev/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
