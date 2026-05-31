b968cdab6fb5314eae15652520f3622d4f4c6e2f
hugo-sants <hugovictorsantos30@gmail.com>
2026-05-30 21:13:34 -0300

    feat: add barbershop service

---
+++ projeto-novo/backend/core/src/main/java/com/dev/core/services/barbershop/BarbershopService.java
@@ -0,0 +1,127 @@
+package com.dev.core.services.barbershop;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.core.dtos.barbershop.BarbershopRegisterDTO;
+import com.dev.core.dtos.barbershop.BarbershopResponseDTO;
+import com.dev.core.dtos.barbershop.BarbershopUpdateDTO;
+import com.dev.core.exception.BarbershopNotFoundException;
+import com.dev.core.exception.InvalidBarbershopException;
+import com.dev.core.mapper.AddressMapper;
+import com.dev.core.mapper.BarbershopMapper;
+import com.dev.core.models.barbershop.Address;
+import com.dev.core.models.barbershop.Barbershop;
+import com.dev.core.repositories.BarbershopRepository;
+
+@Service
+public class BarbershopService {
+
+    @Autowired
+    private BarbershopRepository repository;
+
+    @Autowired
+    private BarbershopMapper barbershopMapper;
+
+    @Autowired
+    private AddressMapper addressMapper;
+    
+    public BarbershopResponseDTO registerBarbershop(BarbershopRegisterDTO dto) throws InvalidBarbershopException {
+        if (!validateBarbershopParams(dto)) {
+            throw new InvalidBarbershopException();
+        }
+
+        Barbershop barbershop = barbershopMapper.toEntity(dto);
+
+        Barbershop saved = repository.save(barbershop);
+
+        return barbershopMapper.toResponse(saved);
+    }
+
+    public BarbershopResponseDTO deleteBarbershop(UUID id){
+        Optional<Barbershop> optional = repository.findById(id);
+
+        if (optional.isEmpty()) {
+            throw new BarbershopNotFoundException();
+        }
+
+        repository.deleteById(id);
+        
+        return barbershopMapper.toResponse(optional.get());
+    }
+
+    public BarbershopResponseDTO updateBarbershop(UUID id, BarbershopUpdateDTO dto) throws BarbershopNotFoundException{
+        Optional<Barbershop> optional = repository.findById(id);
+
+        if (optional.isEmpty()) {
+            throw new BarbershopNotFoundException();   
+        }
+
+        if (validateBarbershopParams(dto)) {
+            optional.get().setName(dto.name());
+            optional.get().setUsername(dto.username());
+
+            Address location = optional.get().getLocation();
+            Address newLocation = addressMapper.toEntity(dto.location());
+            
+            location.setStreet(newLocation.getStreet());
+            location.setNumber(newLocation.getNumber());
+            location.setNeighborhood(newLocation.getNeighborhood());
+            location.setCity(newLocation.getCity());
+            location.setUf(newLocation.getUf());
+
+            optional.get().setLocation(location);
+        }
+
+        repository.save(optional.get());
+
+        return barbershopMapper.toResponse(optional.get());
+    }
+
+    public BarbershopResponseDTO getBarbershop(UUID id) throws BarbershopNotFoundException{
+        Optional<Barbershop> optional = repository.findById(id);
+
+        if (optional.isEmpty()) {
+            throw new BarbershopNotFoundException();
+        }
+
+        return barbershopMapper.toResponse(optional.get());
+    }
+
+    public List<BarbershopResponseDTO> getAllBarbershops() {
+        return repository.findAll()
+                .stream()
+                .map(barbershopMapper::toResponse)
+                .toList();
+    }
+
+    private boolean validateBarbershopParams(BarbershopRegisterDTO dto){
+        Boolean result = true;
+
+        if (dto.name() == null || dto.name().isBlank()) {
+            result = false;
+        }
+        if (dto.username() == null || dto.username().isBlank()) {
+            result = false;
+        }
+
+        return result;
+    }
+    
+    private boolean validateBarbershopParams(BarbershopUpdateDTO dto){
+        Boolean result = true;
+
+        if (dto.name() == null || dto.name().isBlank()) {
+            result = false;
+        }
+        if (dto.username() == null || dto.username().isBlank()) {
+            result = false;
+        }
+
+        return result;
+    }
+}
