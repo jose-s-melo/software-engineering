cfad6426e39a349836c7c8aa2b0887d2b9e210d3
hugo-sants <hugovictorsantos30@gmail.com>
2026-06-03 13:27:48 -0300

    docs: add Barbershop DTO documentation

--- projeto-novo/backend/core/src/main/java/com/dev/core/dtos/barbershop/BarbershopUpdateDTO.java
+++ projeto-novo/backend/core/src/main/java/com/dev/core/dtos/barbershop/BarbershopUpdateDTO.java
@@ -2,9 +2,30 @@ package com.dev.core.dtos.barbershop;
 
 import java.io.Serializable;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Dados para atualização de uma barbearia")
 public record BarbershopUpdateDTO(
+    @Schema(
+        description = "Novo nome da barbearia",
+        example = "Barbearia Novo Exemplo"
+    )
     String name,
+
+    @Schema(
+        description = "Novo nome de usuário da barbearia",
+        example = "barbearia-novo-exemplo"
+    )
     String username,
+
+    @Schema(
+        description = "Novo endereço da barbearia"
+    )
     AddressDTO location,
+
+    @Schema(
+        description = "Novo telefone da barbearia para contato",
+        example = "(21) 7777-76666"
+    )
     String phone
 ) implements Serializable {}
