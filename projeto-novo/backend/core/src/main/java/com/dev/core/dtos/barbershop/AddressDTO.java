dbe0a676efb1ae385deeb62ce9501e33b33b1678
hugo-sants <hugovictorsantos30@gmail.com>
2026-06-03 13:28:23 -0300

    docs: add address DTO documentation

--- projeto-novo/backend/core/src/main/java/com/dev/core/dtos/barbershop/AddressDTO.java
+++ projeto-novo/backend/core/src/main/java/com/dev/core/dtos/barbershop/AddressDTO.java
@@ -2,10 +2,39 @@ package com.dev.core.dtos.barbershop;
 
 import java.io.Serializable;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Dados de endereço de uma barbearia")
 public record AddressDTO(
+
+    @Schema(
+        description = "Nome da rua",
+        example = "Rua João Pessoa"
+    )
     String street,
+
+    @Schema(
+        description = "Número do imóvel",
+        example = "150"
+    )
     Long number,
+
+    @Schema(
+        description = "Bairro",
+        example = "Centro"
+    )
     String neighborhood,
+
+    @Schema(
+        description = "Cidade",
+        example = "Campina Grande"
+    )
     String city,
+
+    @Schema(
+        description = "Unidade Federativa (UF)",
+        example = "PB"
+    )
     String uf
+
 ) implements Serializable {}
--- projeto-novo/backend/core/src/main/java/com/dev/core/dtos/barbershop/AddressResponseDTO.java
+++ projeto-novo/backend/core/src/main/java/com/dev/core/dtos/barbershop/AddressResponseDTO.java
@@ -3,11 +3,45 @@ package com.dev.core.dtos.barbershop;
 import java.io.Serializable;
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Resposta contendo os dados de um endereço cadastro")
 public record AddressResponseDTO(
+
+    @Schema(
+        description = "Identificador único do endereço",
+        example = "550e8400-e29b-41d4-a716-446655440000"
+    )
     UUID id,
+
+    @Schema(
+        description = "Nome da rua",
+        example = "Rua João Pessoa"
+    )
     String street,
+
+    @Schema(
+        description = "Número do imóvel",
+        example = "150"
+    )
     Long number,
+
+    @Schema(
+        description = "Bairro",
+        example = "Centro"
+    )
     String neighborhood,
+
+    @Schema(
+        description = "Cidade",
+        example = "Campina Grande"
+    )
     String city,
+
+    @Schema(
+        description = "Unidade Federativa (UF)",
+        example = "PB"
+    )
     String uf
+
 ) implements Serializable {}
