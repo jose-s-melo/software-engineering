e2c9d2e4fe12efda31080b8fa3f12648ebf80590
hugo-sants <hugovictorsantos30@gmail.com>
2026-06-25 14:33:31 -0300

    feat: service DTOs

---
+++ projeto-novo/backend/core/src/main/java/com/dev/core/dtos/serviceoffering/ServiceOfferingResponseDTO.java
@@ -0,0 +1,52 @@
+package com.dev.core.dtos.serviceoffering;
+
+import com.dev.core.dtos.barbershop.BarbershopResponseDTO;
+import com.dev.core.models.serviceoffering.ServiceOffering;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+/**
+ * DTO for {@link ServiceOffering}
+ */
+@Schema(description = "Dados de retorno de um serviço da barbearia")
+public record ServiceOfferingResponseDTO(
+
+        @Schema(
+                description = "Identificador do serviço"
+        )
+        UUID id,
+
+        @Schema(
+                description = "Nome do serviço",
+                example = "Corte masculino"
+        )
+        String name,
+
+        @Schema(
+                description = "Categoria do serviço",
+                example = "Cabelo"
+        )
+        String category,
+
+        @Schema(
+                description = "Descrição opcional do serviço",
+                example = "Corte tradicional com máquina e tesoura"
+        )
+        String description,
+
+        @Schema(
+                description = "Preço do serviço",
+                example = "25.00"
+        )
+        BigDecimal price,
+
+        @Schema(
+                description = "Indica se o serviço está ativo",
+                example = "true"
+        )
+        Boolean active
+
+) implements Serializable {}
