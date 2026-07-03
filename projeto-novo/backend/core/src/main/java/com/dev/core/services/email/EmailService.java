6f2a7e0cab73678b5667711f4403a8798325fe19
hugo-sants <hugovictorsantos30@gmail.com>
2026-07-03 18:46:36 -0300

    feat: add email service

---
+++ projeto-novo/backend/core/src/main/java/com/dev/core/services/email/EmailService.java
@@ -0,0 +1,58 @@
+package com.dev.core.services.email;
+
+import com.dev.core.dtos.email.SendEmailDTO;
+import com.dev.core.mappers.EmailMapper;
+import com.dev.core.models.email.Email;
+import com.dev.core.models.email.StatusEmail;
+import com.dev.core.repositories.EmailRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private EmailRepository repository;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private EmailMapper mapper;
+
+    @Value("${MAIL_USERNAME}")
+    private String emailFrom;
+
+    @Transactional
+    public Email sendEmail(SendEmailDTO dto) {
+
+        Email email = mapper.toEntity(dto);
+
+        email.setSendDateEmail(LocalDateTime.now());
+        email.setEmailFrom(emailFrom);
+
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setTo(email.getEmailTo());
+            message.setSubject(email.getSubject());
+            message.setText(email.getText());
+
+            mailSender.send(message);
+
+            email.setStatusEmail(StatusEmail.SENT);
+
+        } catch (MailException e) {
+            email.setStatusEmail(StatusEmail.ERROR);
+        }
+
+        return repository.save(email);
+    }
+}
